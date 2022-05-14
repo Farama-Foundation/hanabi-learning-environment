1596485931d3e1ee0547b2666f0d0dc9ad49797d
Jet <38184875+jjshoots@users.noreply.github.com>
2022-05-14 17:23:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
+import pathlib
+from setuptools import setup
 from skbuild import setup
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
 setup(
-    name='hanabi_learning_environment',
+    name='jjshoots_test_hanabi',
     version='0.0.1',
     description='Learning environment for the game of hanabi.',
-    author='deepmind/hanabi-learning-environment',
+    author='taijunjet@hotmail.com',
     packages=['hanabi_learning_environment', 'hanabi_learning_environment.agents'],
     install_requires=['cffi']
+    license="MIT",
+    install_requires=["cffi"],
 )
