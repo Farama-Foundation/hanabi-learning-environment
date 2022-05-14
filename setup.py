33996e1cf3e004086092e8a6e1c4f2f9427eabc2
Jordan Terry <jkterry0@gmail.com>
2022-05-14 13:18:28 -0400

    bump version again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from skbuild import setup
 
 setup(
     name='hanabi_learning_environment',
-    version='0.0.2',
+    version='0.0.3',
     description='Learning environment for the game of hanabi.',
     author='deepmind/hanabi-learning-environment',
     packages=['hanabi_learning_environment', 'hanabi_learning_environment.agents'],
